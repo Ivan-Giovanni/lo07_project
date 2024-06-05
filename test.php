927282ca659e540f28e4bf04499a7c5bd141d194
DjouweDanielle <djouweadriennedanielle@gmail.com>
2024-06-05 10:29:06 +0200

    zazazazazazaza

--- test.php
+++ test.php
@@ -12,7 +12,7 @@ $query_string = $_SERVER['QUERY_STRING'];
 // une table de hachage (clé + valeur)
 parse_str($query_string, $param);
 
-// modifazazzezezezeze
+// modifazazzezezezeze modfefefefefef
 
 // --- $action contient le nom de la méthode statique recherchée
 $action = htmlspecialchars($param["action"]);
